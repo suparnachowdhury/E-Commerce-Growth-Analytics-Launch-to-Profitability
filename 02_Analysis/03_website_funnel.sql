fd57750c26a8cf641c9db5afad6854570126b802
Suparna Chowdhury <suchow32@gmail.com>
2026-05-29 16:03:11 -0400

    Update 03_website_funnel.sql

--- 02_Analysis/03_website_funnel.sql
+++ 02_Analysis/03_website_funnel.sql
@@ -97,73 +97,140 @@ WHERE pageview_url = '/home-v2';
 
 -- A/B TEST: Bounce rate comparison (/home vs /home-v2)
 -- Traffic: gsearch nonbrand
--- Time window: 2012-06-19 to 2012-07-28
+-- Time window: 2023-04-02 to 2023-04-02
 
--- STEP 1: Filter test sessions
-CREATE TEMPORARY TABLE test_sessions AS
-SELECT 
-    website_session_id,utm_source,utm_campaign
-    created_at
-FROM website_sessions
-WHERE created_at >= '2023-04-02'
-  AND created_at < '2023-05-15'
-  AND utm_source = 'g_search'
-  AND utm_campaign = 'nonbrand';
+-- STEP 2: Finding the landing page sessions which are the sessions with
+-- minimum pageview_ids.
 
--- select * from test_sessions;
--- STEP 2: Identify landing page per session (first pageview)
-CREATE TEMPORARY TABLE session_landing AS
+CREATE TEMPORARY TABLE first_pv_per_session
 SELECT
-    pv.website_session_id,
-    pv.pageview_url AS landing_page
+	pv.website_session_id,
+    MIN(pv.website_pageview_id) AS first_pv
 FROM website_pageviews pv
-INNER JOIN test_sessions ts
-    ON pv.website_session_id = ts.website_session_id
-INNER JOIN (
-    SELECT
-        website_session_id,
-        MIN(website_pageview_id) AS first_pv_id
-    FROM website_pageviews
-    GROUP BY website_session_id
-) fp
-    ON pv.website_session_id = fp.website_session_id
-   AND pv.website_pageview_id = fp.first_pv_id
-WHERE pv.pageview_url IN ('/home', '/home-v2');
+INNER JOIN website_sessions ws 
+ON pv.website_session_id = ws.website_session_id
+WHERE pv.created_at >= '2023-04-02'
+  AND pv.created_at < '2023-05-15'
+  AND ws.utm_source = 'g_search'
+  AND ws.utm_campaign = 'nonbrand'
+GROUP BY 
+	pv.website_session_id;
+    
+    
+-- STEP 3: Filtering out only the home and lander-1 landing pages
+    
+CREATE TEMPORARY TABLE sessions_landing_page
+SELECT 
+		pv.website_session_id,
+        pv.pageview_url as landing_page
+FROM first_pv_per_session fs
+LEFT JOIN   website_pageviews pv
+ON pv.website_pageview_id = fs.first_pv
+WHERE pv.pageview_url  IN ('/home','/home-v2') ;
 
+-- STEP 4: then we count pageviews per session limiting to bounced sessions
 
--- STEP 3: Count pageviews per session
-CREATE TEMPORARY TABLE session_pageviews AS
+CREATE TEMPORARY TABLE only_bounced_session AS         
 SELECT
-    website_session_id,
-    COUNT(*) AS pageviews
-FROM website_pageviews
-GROUP BY website_session_id;
+		sh.website_session_id,
+        sh.landing_page,
+        COUNT(pv.website_pageview_id) AS count_of_page_viewed
+FROM sessions_landing_page sh
+LEFT JOIN website_pageviews pv
+ON sh.website_session_id = pv.website_session_id
+GROUP BY
+		sh.website_session_id,
+        sh.landing_page
+HAVING	
+		 COUNT(pv.website_pageview_id) = 1;
+         
+-- Final output
+SELECT 
+	 sessions_landing_page.landing_page,
+     COUNT(sessions_landing_page.website_session_id) AS sessions,
+     COUNT( only_bounced_session.website_session_id) AS bounced_sessions,
+     COUNT( only_bounced_session.website_session_id)/
+     COUNT(sessions_landing_page.website_session_id) * 100.0 AS bounce_rate
+FROM sessions_landing_page 
+LEFT JOIN only_bounced_session
+ON sessions_landing_page.website_session_id = only_bounced_session.website_session_id
+GROUP BY sessions_landing_page.landing_page;
 
 
--- STEP 4: Identify bounced sessions
-CREATE TEMPORARY TABLE bounced_sessions AS
-SELECT
-    website_session_id
-FROM session_pageviews
-WHERE pageviews = 1;
-
-
--- STEP 5: Final A/B test result
-SELECT
-    sl.landing_page,
-    COUNT(*) AS sessions,
-    SUM(CASE 
-            WHEN bs.website_session_id IS NOT NULL THEN 1 
-            ELSE 0 
-        END) AS bounced_sessions,
-    SUM(CASE 
-            WHEN bs.website_session_id IS NOT NULL THEN 1 
-            ELSE 0 
-        END) * 1.0 / COUNT(*) AS bounce_rate
-FROM session_landing sl
-LEFT JOIN bounced_sessions bs
-    ON sl.website_session_id = bs.website_session_id
-GROUP BY sl.landing_page;
 /*
-Build a full conversion funnel from /lander-1 through to the thank-you page using data from August 5th onward. I want to see where we're losing people at each step so we know what to fix next.
+LANDING PAGE TREND ANALYSIS
+Website Manager: 
+Could you pull the volume of paid search nonbrand traffic landing 
+on /home and /home-v2, trended weekly since June 1st? 
+I want to confirm the traffic is all routed correctly.
+Could you also pull our overall paid search bounce rate trended weekly? 
+I want to make sure the lander change has improved the overall picture.
+*/
+
+
+SELECT * FROM website_sessions LIMIT 5;
+SELECT   DISTINCT utm_content FROM website_sessions LIMIT 5;
+
+-- STEP 1: Finding the landing page sessions which are the sessions with
+-- minimum pageview_ids.
+
+ 
+ CREATE TEMPORARY TABLE session_w_min_pv_id_view_count
+ SELECT    
+	pv.website_session_id,
+    MIN(pv.website_pageview_id) AS first_pv,
+    COUNT(pv.website_pageview_id) AS pageview_counts
+FROM website_pageviews pv
+INNER JOIN website_sessions ws 
+ON pv.website_session_id = ws.website_session_id
+WHERE pv.created_at >= '2012-06-01'
+AND pv.created_at <'2012-08-31'
+AND ws.utm_campaign = 'nonbrand'
+AND ws.utm_source = 'gsearch'
+GROUP BY
+		pv.website_session_id;
+        
+        
+SELECT * FROM session_w_min_pv_id_view_count LIMIT 5;
+    
+-- STEP 3: Filtering out only the home and lander-1 landing pages
+    
+CREATE TEMPORARY TABLE sessions_w_landing_page_created_at
+SELECT 
+		fs.*,
+        pv.created_at,
+        pv.pageview_url as landing_page
+FROM session_w_min_pv_id_view_count fs
+LEFT JOIN   website_pageviews pv
+ON pv.website_pageview_id = fs.first_pv;
+
+SELECT * FROM sessions_w_landing_page_created_at LIMIT 5;
+-- STEP 4: then we count pageviews per session limiting to bounced sessions
+
+
+         
+
+-- Final output
+
+SELECT 
+	 YEARWEEK(created_at) AS year_week,
+     MIN(DATE(created_at)) AS week_start_date,
+     COUNT(DISTINCT website_session_id) AS total_sessions,
+     COUNT(DISTINCT CASE WHEN pageview_counts = 1 
+				THEN website_session_id ELSE NULL END ) AS bounced_sessions,
+	COUNT(DISTINCT CASE WHEN pageview_counts = 1 
+				THEN website_session_id ELSE NULL END )/
+                COUNT(DISTINCT website_session_id)*100.0 AS bounced_rate,
+     COUNT(CASE WHEN landing_page = '/home' 
+				THEN website_session_id ELSE NULL END ) AS home_sessions,
+	COUNT(CASE WHEN landing_page = '/lander-1' 
+				THEN website_session_id ELSE NULL END ) AS lander1_sessions
+FROM sessions_w_landing_page_created_at
+GROUP BY YEARWEEK(created_at);
+
+/*
+Build a full conversion funnel from /home-v2 through to the thank-you page using data 
+from August 5th onward. 
+
+I want to see where we're losing people at each step so we know what to fix next.
 */
